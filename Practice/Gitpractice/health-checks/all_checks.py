d904d36d8b1679790f56d69e2db84970b2048c3e
abizada <abizada@users.noreply.github.com>
2025-10-02 15:36:10 +0300

    Add health-checks repo into Gitpractice

---
+++ Practice/Gitpractice/health-checks/all_checks.py
@@ -0,0 +1,76 @@
+#!/usr/binenv python3
+
+import os
+import sys
+import shutil
+import socket
+import psutil
+
+def check_reboot():
+    """Return True if the computer has a pending reboot"""
+    return os.path.exists("reboot-required")
+
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
+def main():
+    checks = [
+            (check_reboot, "Pending Reboot."),
+            (check_root_full, "Root partition full"),
+            (check_no_network, "No working network."),
+            ]
+
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""    
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk = "/", min_gb = 2,min_percent = 10 )
+
+def main(): 
+    checks = [
+        (check_reboot,"Pending reboot"), 
+        (check_root_full, "Root partition full"),
+    ]
+
+    everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
+        
+    if check_reboot(): 
+        print("Pending Reboot.")
+        sys_exit(1)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("Root partition full.")
+        sys.exit(1)
+    
+    print("Everything ok")
+    sys.exit(0)
+
+main()
